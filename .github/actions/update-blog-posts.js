0154e9bc85fbc6541b808f22c0c2b08c323c9428
Jason Etcovitch <jasonetco@github.com>
2020-06-07 19:05:20 -0400

    Automatically update blog posts section

---
+++ .github/actions/update-blog-posts.js
@@ -0,0 +1,25 @@
+const { Toolkit } = require('actions-toolkit')
+const { ReadmeBox } = require('readme-box')
+const Parser = require('rss-parser')
+const parser = new Parser()
+
+const MAX_ITEMS = 5
+
+Toolkit.run(async tools => {
+  // Fetch feed
+  const feed = await parser.parseURL(process.env.FEED_URL)
+
+  // Create our new list
+  const newString = feed.items
+    .slice(0, MAX_ITEMS)
+    .map(item => `* [${item.title}](${item.link})`).join('\n')
+
+  // Update the section of our README
+  await ReadmeBox.updateSection(newString, {
+    ...tools.context.repo,
+    token: tools.token,
+    section: 'posts'
+  })
+}, {
+  secrets: ['GITHUB_TOKEN', 'FEED_URL']
+})
